6210229093ae30b57de310d893009e7d469be5a2
asif adham <asif@alagzoo.com>
2024-09-11 05:34:31 +0600

    type added

--- src/package/login/login.component.tsx
+++ src/package/login/login.component.tsx
@@ -2,14 +2,50 @@ import { Button } from "react-bootstrap";
 import FormBuilderComponent, { FormConfig } from "../form/form-builder/form-builder.component";
 import * as yup from "yup";
 import './login.scss';
-import { FC } from "react";
+import { FC, RefObject } from "react";
 
 interface LoginProps {
-    formRef: any;
-    initialValues: any;
+    formRef: RefObject<unknown>;
+    initialValues: { [key: string]: string };
     formConfig: FormConfig;
-    validationSchema: any;
-    loginAction: any;
+    validationSchema: yup.ObjectSchema<any>;
+    loginAction: {
+        variant: string; 
+        label: string; 
+        event: () => void; 
+    };
+}
+
+
+const styles = {
+    loginContainer: {
+        height: '100vh'
+    },
+    loginPageBacksplash: {
+        width: '100vw',
+        height: '60vh',
+        background: 'linear-gradient(to right, #ebebeb, #9e9e9e)',
+        borderBottomLeftRadius: '55% 71%',
+        borderBottomRightRadius: '54% 71%',
+        overflow: 'hidden'
+    },
+    loginCardWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    loginCard: {
+        borderRadius: '5px',
+        width: '30rem',
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '-10rem',
+        padding: '1rem'
+    },
+    loginFont: {
+        fontSize: '18px',
+        fontWeight: '700'
+    }
 }
 
 
@@ -22,37 +58,19 @@ const Login: FC<LoginProps> = ({
 }) => {
     return (
         <>
-            <div className="login-container" style={{
-                height: '100vh'
-            }}>
-                <div className="login-page-backsplash" style={
-                    {
-                        width: '100vw',
-                        height: '60vh',
-                        background: 'linear-gradient(to right, #ebebeb, #9e9e9e)',
-                        borderBottomLeftRadius: '55% 71%',
-                        borderBottomRightRadius: '54% 71%',
-                        overflow: 'hidden'
-                      }
-                }></div>
-                <div className="login-card-wrapper p-2" style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
+            <div className="login-container" style={styles.loginContainer}>
+                <div className="login-page-backsplash" style={styles.loginPageBacksplash}></div>
+                <div className="login-card-wrapper p-2" style={styles.loginCardWrapper}>
                     <div className="bg-light shadow-lg" style={{
-                        borderRadius: '5px',
-                        width: '30rem',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        marginTop: '-10rem',
-                        padding: '1rem'
-                    }}>
+        borderRadius: '5px',
+        width: '30rem',
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '-10rem',
+        padding: '1rem'
+    }}>
                         <div>
-                            <div style={{
-                                fontSize: '18px',
-                                fontWeight: '700'
-                            }}>
+                            <div style={styles.loginFont}>
                                 Login to test system
                             </div>
                         </div>
